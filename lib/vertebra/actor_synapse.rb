7129c9e17dfadbcc477fbb6c32cb1c9a495a9b37
Kirk Haines <khaines@engineyard.com>
2009-03-02 14:21:34 -0700

    A subclass of Vertebra::Synapse.

---
+++ lib/vertebra/actor_synapse.rb
@@ -0,0 +1,33 @@
+module Vertebra
+  class ActorSynapse < Synapse
+    
+    def initialize(agent)
+      @agent = agent
+      super()
+    end
+    
+    def action(*args, &block)
+      condition do
+        begin
+          error = nil
+          result = block.call(self, *args)
+        rescue Exception => e
+          error = e
+        end
+        
+        if Vertebra::Synapse === result
+          # The action returned a deferrable, so it'll queue the deferrable
+          result.set_deferred_status = nil
+          @agent.enqueue_synapse(result)
+          :deferred
+        else
+          self[:results] = result unless error
+          self[:error] = error if error
+          :succeeded
+        end
+      end
+      
+    end
+  end
+end
+  
