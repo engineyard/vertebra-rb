03ec437b1bb41806068652fba9578c30602da38f
Samuel Tesla <stesla@engineyard.com>
2009-03-13 14:40:01 -0500

    Changed options parsing over to use optparse, also got config-file and CLI option cascade working correctly.

--- lib/vertebra/vertebra_cli.rb
+++ lib/vertebra/vertebra_cli.rb
@@ -18,6 +18,7 @@
 # This file is a reimplementation of xmpp4r's rexmladdons. As such, it depends
 # on some parts of REXML.
 
+require 'optparse'
 require 'rubygems'
 require 'vertebra'
 require 'vertebra/agent'
@@ -39,25 +40,10 @@ module Vertebra
   end
 
   class VertebraCLI
-    def self.read_config_file
-      @opts = {}
-      cfg = nil
-      if String === @config_file
-        path = File.expand_path(@config_file)
-        if File.exist? path
-          cfg = File.read(path)
-        end
-      elsif @config_file.respond_to?(:read)
-        cfg = @config_file.read
-      end
-      if cfg
-        @opts = YAML.load(cfg)
-        @jid ||= @opts['jid']
-        @password ||= @opts['password']
-        @opts.delete('jid')
-        @opts.delete('password')
-        @opts = keys_to_symbols(@opts)
-      end
+    def self.read_config_file(filename)
+      path = File.expand_path(filename)
+      options = YAML.load(File.read(path))
+      keys_to_symbols(options)
     end
 
     # Very little command line parsing is done.  A check will be made for a
@@ -66,131 +52,105 @@ module Vertebra
     # be assumed to be the operation to invoke.
 
     def self.parse_commandline
-      @scope = :all
-      @config_file = '~/.vertebra/vertebra'
-      @client = nil
-      @jid = nil
-      @password = nil
-      @verbose = false
-      @yaml = true
-      @enable_logging = false
-      @discovery_only = false
+      options ={:config_file => '~/.vertebra/vertebra',
+                :scope => :all,
+                :yaml => true}
+      OptionParser.new do |opts|
+        opts.banner = "vertebra /OP [options] [arguments]"
 
-      # Default to showing help
-      ARGV[0] = '--help' if ARGV.empty?
+        opts.on('--single',
+                'Dispatch the op with a scope of \'single\'.') do
+          options[:scope] = :single
+        end
 
-      # First, find the few command line flags that need special handling.
-      args_to_remove = []
-      ARGV.each_with_index do |arg, idx|
-        case arg
-        when '--single'
-          @scope = :single
-          args_to_remove << idx
-        when '--all'
-          @scope = :all
-          args_to_remove << idx
-        when '--config'
-          @config_file = ARGV[idx + 1] if ARGV[idx + 1]
-          args_to_remove << idx
-          args_to_remove << idx + 1
-        when '--jid'
-          @jid = ARGV[idx + 1] if ARGV[idx + 1]
-          args_to_remove << idx
-          args_to_remove << idx + 1
-        when '--password'
-          @password = ARGV[idx + 1] if ARGV[idx + 1]
-          args_to_remove << idx
-          args_to_remove << idx + 1
-        when '--verbose'
-          @verbose = true
-          args_to_remove << idx
-        when '--inspect'
-          @yaml = false
-          args_to_remove << idx
-        when '--yaml'
-          @yaml = true
-          args_to_remove << idx
-        when '--log'
-          @enable_logging = true
-          args_to_remove << idx
-        when '--discover'
-          @discovery_only = true
-          args_to_remove << idx
-        when '--herault-jid'
-          @opts[:herault_jid] = ARGV[idx + 1] if ARGV[idx + 1]
-          args_to_remove << idx
-          args_to_remove << idx + 1
-        when '--help'
-          puts <<EHELP
-vertebra /OP [vertebra-flags] [op arguments]
-        --all             Dispatch the op with a scope of 'all'.  This is the default.
-        --single          Dispatch the op with a scope of 'single'.
-        --config FILENAME Specify a config file to use. If not specified, this
-                                                                                defaults to HOME/.vertebra/vertebra
-        --jid JID         The JID to use to connect to vertebra. This overrides
-                                                                                anything specified in the configuration file.
-        --password PWD    The password to use with the jid to connect to vertebra.
-        --herault-jid JID The JID of the herault instance to query for discovery.
-        --yaml            Transform the op results to YAML before displaying them.
-                                                                                This tends to make them more human readable, and is the
-                                                                                default.
-        --inspect         Display the results of the op in the Ruby inspect format.
-        --log             Turn logging on. This will write an agent.PID.log file to
-                                                                                the temp directory, logging the CLI actions. Off by default.
-        --discover        Do discovery only. This is primarily a developer tool.
-        --help            Show this text.
+        opts.on('--all',
+                'Dispatch the op with a scope of \'all\'.',
+                '   (default)') do
+          options[:scope] = :all
+        end
 
-Anything on the command line that is not one of the above flags is passed to
-the operation.  In order to differentiate between resources and other strings
-on the command line, one should preface any resource with 'res:'.
+        opts.on('--config FILENAME',
+                'Specify a config file to use.',
+                '   (defaults to ~/.vertebra/vertebra)') do |v|
+          options[:config_file] = v
+        end
 
-i.e.
+        opts.on('--jid JID',
+                'The JID to use to connect to XMPP.') do |v|
+          options[:jid] = v
+        end
 
-vertebra /gem/list res:/cluster/rd00 res:/slice/0
+        opts.on('--password PASSWORD',
+                'The password for the XMPP JID.') do |v|
+          options[:password] = v
+        end
 
-All resources that are provided on the command line will be used for discovery.
+        opts.on('-v', '--[no-]verbose', "Toggle verbose mode") do |v|
+          options[:verbose] = v
+        end
 
-Primarily as a developer aid, one may also provide specific jids:
+        opts.on('--inspect',
+                'Display results in the Ruby inspect format.') do |v|
+          options[:yaml] = !v
+        end
 
-vertebra /gem/list jid:rd00-s00000@localhost/agent
-EHELP
+        opts.on('--yaml',
+                'Display results as YAML.',
+                '   (default)') do |v|
+          options[:yaml] = v
+        end
+
+        opts.on('--log', 'Enable logging') do |v|
+          options[:enable_logging] = v
+        end
+
+        opts.on('--discover',
+                'Do discovery only.',
+                '   (This is primarily a developer tool.)') do |v|
+          options[:discovery_only] = v
+        end
+
+        opts.on('--herault-jid JID',
+                'The JID for Herault') do |v|
+          options[:herault_jid] = v
+        end
+
+        opts.on('-?', '-h', '--help') do
+          puts opts
           exit
         end
-      end
-
-      Vertebra::disable_logging unless @enable_logging
-
-      args_to_remove.each {|arg_idx| ARGV[arg_idx] = :gone}
-      intermediate_args = ARGV.reject {|arg| arg == :gone}
+      end.parse!
 
       # Pull the op
-      @op = intermediate_args.shift
+      options[:op] = ARGV.shift
 
       # Now search the rest of the args to identify the resources
 
-      @parsed_args = []
-      intermediate_args.each do |arg|
+      op_args = []
+      ARGV.each do |arg|
         if arg =~ /string:([^\s]*)/
-          @parsed_args << $1
+          op_args << $1
         elsif arg =~ /string:(["'])([^\1]*)/ # TODO: This regexp can be improved.
-          @parsed_args << $2
+          op_args << $2
         elsif arg =~ /res:([^\s]*)/
-          @parsed_args << Vertebra::Resource.new($1)
+          op_args << Vertebra::Resource.new($1)
         else
-          @parsed_args << arg
+          op_args << arg
         end
       end
+      options[:op_args] = op_args
+      options
     end
 
     def self.dispatch_request
-      puts "Initializing agent with #{@jid}/#{@password}" if @verbose
-      agent = Vertebra::Agent.new(@jid, @password, @opts)
+      puts "Initializing agent with #{@jid}:#{@password}" if @verbose
+      agent = Vertebra::Agent.new(@jid, @password, @options)
 
       EM.next_tick do
 #        EM.add_timer(10 / 1000) do
           if @discovery_only
-            puts "Doing discovery #{[@op,@parsed_args].flatten.inspect}" if @verbose
-            resources = @parsed_args.select {|r| Vertebra::Resource == r}
+            puts "Doing discovery #{[@op,@op_args].flatten.inspect}" if @verbose
+            resources = @op_args.select {|r| Vertebra::Resource == r}
             @client = agent.discover(@op,*resources)
             @check_timer = EM::PeriodicTimer.new(50 / 1000) do
               if @client.done?
@@ -201,8 +161,8 @@ EHELP
               end
             end
           else
-            puts "Making request for #{@op} #{@scope} #{@parsed_args.inspect}" if @verbose
-            request = agent.request(@op,@scope,*@parsed_args)
+            puts "Making request for #{@op} #{@scope} #{@op_args.inspect}" if @verbose
+            request = agent.request(@op,@scope,*@op_args)
             @check_timer = EM::PeriodicTimer.new(50 / 1000) do
               if request[:results]
                 agent.stop
@@ -227,8 +187,15 @@ EHELP
     end
 
     def self.run
-      parse_commandline
-      read_config_file
+      cli_options = parse_commandline
+      file_options = read_config_file(cli_options.delete :config_file)
+      @options = file_options.merge cli_options
+      Vertebra::disable_logging unless @options.delete :enable_logging
+      ## TODO: Fix this so that we don't assign an asston of fields.
+      [:discovery_only, :jid, :op, :op_args,
+       :password, :scope, :verbose, :yaml].each do |option|
+        instance_variable_set("@#{option}", @options.delete(option))
+      end
       dispatch_request
     end
 
